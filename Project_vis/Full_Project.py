ec49293ab6035b7c7a154d8c7b75752662475579
SensvanAert <r0878915@.student.thomasmore.be>
2022-05-03 15:43:13 +0200

    progress

--- Project_vis/Full_Project.py
+++ Project_vis/Full_Project.py
@@ -1,6 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 import threading
+import cgitb ; cgitb.enable() 
+import spidev 
+import busio
+import digitalio
+import board
+import requests
+from adafruit_bus_device.spi_device import SPIDevice
 
 GPIO.setmode(GPIO.BCM)
 #Depth pins
@@ -10,55 +17,72 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(25, GPIO.IN)
 GPIO.setup(8, GPIO.OUT)
 #Light pins
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.OUT)
+GPIO.setup(20, GPIO.IN)
+GPIO.setup(21, GPIO.OUT)
 #Feeder pins
 GPIO.setup(27, GPIO.IN)
 GPIO.setup(22, GPIO.IN)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(9, GPIO.OUT)
 GPIO.setup(1, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
 GPIO.setup(6, GPIO.OUT)
 
+# Initialize SPI bus
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Initialize control pins for adc
+cs0 = digitalio.DigitalInOut(board.CE0)  # chip select CE0 for adc
+adc = SPIDevice(spi, cs0, baudrate= 1000000)
+
 exit_event = threading.Event()
+url = 'http://sensvanaert.hub.ubeac.io/iotesssensvanaert'
+uid = 'iotesssensvanaert'
+toggle = 0
+pump = 0
+actualDepth = 0
 
 GPIO.output(8, 1)
-GPIO.output(24, 1)
-GPIO.output(7, 0)
+GPIO.output(21, 1)
+GPIO.output(9, 0)
 GPIO.output(1, 0)
 GPIO.output(5, 0)
 GPIO.output(6, 0)
 
 def light ():
-    toggle = 0
+    count = 0
+    global toggle
     while True:
         timestamp = time.localtime()
         current_time =  time.strftime("%H:%M:%S", timestamp)
 
         if current_time == "20:00:00":
-            GPIO.output(24, 0)
+            GPIO.output(21, 0)
             toggle = 1
 
         if current_time == "00:00:00":
-            GPIO.output(24, 1)
+            GPIO.output(21, 1)
             toggle = 0
 
-        if GPIO.input(23) == 0:
-            print("pressed button")
-            if toggle == 0:
-                GPIO.output(24, 0)
+        if GPIO.input(20) == 0:
+            if toggle == 0 and count == 0:
+                GPIO.output(21, 0)
                 toggle = 1
-            else:
-                GPIO.output(24, 1)
+            elif count == 0:
+                GPIO.output(21, 1)
                 toggle = 0
+            count = 1
             time.sleep(0.3)
+
+        if GPIO.input(20) == 1:
+            count = 0
 
         if exit_event.is_set():
             break
 
 def depthAndPump():
+    global pump
     aquariumDepth = 25
-    actualDepth = 0
+    global actualDepth
     depthActive = 0
     buttonActive = 0
 
@@ -78,12 +102,15 @@ def depthAndPump():
         signalLow = time.time()
         timePassed = signalLow - signalHigh
         distance = 17000 * timePassed
-        actualDepth = aquariumDepth - distance
+        actualDepth = round(aquariumDepth - distance, 2)
         print(str(actualDepth))
+
         if depthActive == 1 or buttonActive == 1:
+            pump = 1
             time.sleep(0.2)
         else:
-            time.sleep(1.0)
+            pump = 0
+            time.sleep(0.7)
         
         if (actualDepth < 14 and buttonActive == 0):
             depthActive = 1
@@ -121,17 +148,17 @@ def feeder ():
                 counter +=1
             if counter == 125:
                 enableFeeder = 0
-            GPIO.output(7, 1)
+            GPIO.output(9, 1)
             GPIO.output(1, 1)
             time.sleep(0.001)
-            GPIO.output(7, 0)
+            GPIO.output(9, 0)
             GPIO.output(5, 1)
             time.sleep(0.001)
             GPIO.output(1, 0)
             GPIO.output(6, 1)
             time.sleep(0.001)
             GPIO.output(5, 0)
-            GPIO.output(7, 1)
+            GPIO.output(9, 1)
             time.sleep(0.001)
             GPIO.output(6, 0)
 
@@ -143,26 +170,51 @@ def feeder ():
             GPIO.output(1, 1)
             time.sleep(0.001)
             GPIO.output(5, 0)
-            GPIO.output(7, 1)
+            GPIO.output(9, 1)
             time.sleep(0.001)
             GPIO.output(1, 0)
             GPIO.output(6, 1)
             time.sleep(0.001)
-            GPIO.output(7, 0)
+            GPIO.output(9, 0)
 
         if exit_event.is_set():
             break
 
+def sendData ():
+    while True:
+        data= {
+		    "id": uid,
+		    "sensors":[{
+			    'id': 'light',
+			    'data': toggle
+		    },
+            {
+			    'id': 'pump',
+			    'data': pump
+			},
+            {
+			    'id': 'depth',
+			    'data': actualDepth
+			}
+            ]
+        }
+        requests.post(url, verify=False,  json=data)
+
+        time.sleep(1)
+        if exit_event.is_set():
+            break
 
 # All Threads
 lightThread = threading.Thread(target=light)
 depthThread = threading.Thread(target=depthAndPump)
 feederThread = threading.Thread(target=feeder)
+sendDataThread = threading.Thread(target=sendData)
 
 # Starting the threads
 lightThread.start()
 depthThread.start()
 feederThread.start()
+sendDataThread.start()
 
 try:
     while(True):
@@ -170,7 +222,7 @@ try:
         # current_time = time.strftime("%H:%M:%S", t)
         # print(current_time)
         print("Still working")
-        time.sleep(1.0)
+        time.sleep(3.0)
 
 except KeyboardInterrupt:
     exit_event.set()
